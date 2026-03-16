518d6916b19594be16b47e44d62b132cedc1cdc2
keisuke nagamatsu <ngmt.k0520@gmail.com>
2026-03-16 16:50:49 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!, only:[:create]
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to prototype_path(@comment.prototype)
+    else
+      @prototype = @comment.prototype
+      @comments = @prototype.comments
+      render "prototypes/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+    end
+end
