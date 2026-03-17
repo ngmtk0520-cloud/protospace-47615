a48cf66e1320c1a95b132aa9ea8b763595fe3579
keisuke nagamatsu <ngmt.k0520@gmail.com>
2026-03-17 14:19:02 +0900

    プロトタイプ修正

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,6 @@
 class Prototype < ApplicationRecord
   belongs_to :user
-  validates :title, :catch_copy, :concept, presence: true
+  validates :title, :catch_copy, :concept, :image, presence: true
   has_one_attached :image
   has_many :comments, dependent: :destroy
 end
